5add8afb33e3829e0ec6a7e0a9e00a73ad31559d
prajwal <prajwalkreddy17@gmail.com>
2025-02-06 15:20:59 +0530

    deploy to ec2

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import { prisma } from "@repo/db/prisma"
+
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+
+  try {
+
+    const users = await prisma.user.findMany();
+
+    res.json({ msg: "This is http server written in express.js", users: users })
+
+  } catch (error) {
+    console.log(error)
+    res.json({ msg: "Something went wrong" }).status(400)
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`)
+})
